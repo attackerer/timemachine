e6fb2c619c53cfc638900decd3e482a5c7a71ab1
Mahdi Firouzi Nezamabadi <36706362+mfirouzin@users.noreply.github.com>
2019-11-07 15:48:12 +0330

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,220 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'userinterface.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtGui import QIcon, QPixmap
+import pandas as pd
+
+class ProcessThread(QThread):
+    def __init__(self, UI):
+        QThread.__init__(self)
+        self.UI = UI
+    def __del__(self):
+        self.wait()
+    def run(self):
+        print('run runs')
+        def Def_go_back_func():
+            import sys
+            from _datetime import datetime
+
+            time_tuple = (2000,  # Year
+                          9,  # Month
+                          6,  # Day
+                          0,  # Hour
+                          38,  # Minute
+                          0,  # Second
+                          0,  # Millisecond
+                          )
+
+            def _win_set_time(time_tuple):
+                import win32api
+                dayOfWeek = datetime(*time_tuple).isocalendar()[2]
+                t = time_tuple[:2] + (dayOfWeek,) + time_tuple[2:]
+                win32api.SetSystemTime(*t)
+
+            def _linux_set_time(time_tuple):
+                import subprocess
+                import shlex
+
+                time_string = datetime(*time_tuple).isoformat()
+
+                subprocess.call(shlex.split("timedatectl set-ntp false"))  # May be necessary
+                subprocess.call(shlex.split("sudo date -s '%s'" % time_string))
+                subprocess.call(shlex.split("sudo hwclock -w"))
+
+            if sys.platform == 'linux2' or sys.platform == 'linux':
+                _linux_set_time(time_tuple)
+
+            elif sys.platform == 'win32':
+                _win_set_time(time_tuple)
+        def Def_sync_time_func():
+            import socket
+            import struct
+            import sys
+            import time
+            import datetime
+            import win32api
+
+            # List of servers in order of attempt of fetching
+            server_list = ['ntp.iitb.ac.in', 'time.nist.gov', 'time.windows.com', 'pool.ntp.org']
+
+            '''
+            Returns the epoch time fetched from the NTP server passed as argument.
+            Returns none if the request is timed out (5 seconds).
+            '''
+
+            def gettime_ntp(addr='time.nist.gov'):
+                # http://code.activestate.com/recipes/117211-simple-very-sntp-client/
+                TIME1970 = 2208988800  # Thanks to F.Lundh
+                client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                data = '\x1b' + 47 * '\0'
+                data = data.encode()
+                try:
+                    # Timing out the connection after 5 seconds, if no response received
+                    client.settimeout(5.0)
+                    client.sendto(data, (addr, 123))
+                    data, address = client.recvfrom(1024)
+                    if data:
+                        epoch_time = struct.unpack('!12I', data)[10]
+                        epoch_time -= TIME1970
+                        return epoch_time
+                except socket.timeout:
+                    return None
+
+            if __name__ == "__main__":
+                # Iterates over every server in the list until it finds time from any one.
+                for server in server_list:
+                    epoch_time = gettime_ntp(server)
+                    if epoch_time is not None:
+                        # SetSystemTime takes time as argument in UTC time. UTC time is obtained using utcfromtimestamp()
+                        utcTime = datetime.datetime.utcfromtimestamp(epoch_time)
+                        win32api.SetSystemTime(utcTime.year, utcTime.month, utcTime.weekday(), utcTime.day,
+                                               utcTime.hour, utcTime.minute, utcTime.second, 0)
+                        # Local time is obtained using fromtimestamp()
+                        localTime = datetime.datetime.fromtimestamp(epoch_time)
+                        print("Time updated to: " + localTime.strftime("%Y-%m-%d %H:%M") + " from " + server)
+                        break
+                    else:
+                        print("Could not find time from " + server)
+        if self.UI.pb_go_back_status == 1:
+            Def_go_back_func()
+        if self.UI.pb_sync_time_status == 1:
+            Def_sync_time_func()
+
+
+class Ui_MainWindow(object):
+    pb_go_back_status=0
+    pb_sync_time_status = 0
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(363, 374)
+        MainWindow.setMinimumSize(QtCore.QSize(150, 200))
+        MainWindow.setMaximumSize(QtCore.QSize(1000, 1000))
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pb_go_back = QtWidgets.QPushButton(self.centralwidget)
+        self.pb_go_back.setGeometry(QtCore.QRect(80, 60, 191, 71))
+        font = QtGui.QFont()
+        font.setFamily("Segoe Print")
+        font.setPointSize(14)
+        self.pb_go_back.setFont(font)
+        self.pb_go_back.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
+        self.pb_go_back.setMouseTracking(False)
+        self.pb_go_back.setStyleSheet("#pb_go_back {\n"
+"background: gray;\n"
+"border: 5px rgb(0, 170, 255);\n"
+"border-radius: 12px;\n"
+"background:lightgray;\n"
+"}")
+        self.pb_go_back.setObjectName("pb_go_back")
+        self.pb_sync_time = QtWidgets.QPushButton(self.centralwidget)
+        self.pb_sync_time.setGeometry(QtCore.QRect(80, 170, 191, 71))
+        font = QtGui.QFont()
+        font.setFamily("Segoe Print")
+        font.setPointSize(14)
+        font.setBold(False)
+        font.setWeight(50)
+        self.pb_sync_time.setFont(font)
+        self.pb_sync_time.setStyleSheet("#pb_sync_time {\n"
+"background: gray;\n"
+"border: 5px rgb(0, 170, 255);\n"
+"border-radius: 12px;\n"
+"background:mediumseagreen;\n"
+"}")
+        self.pb_sync_time.setObjectName("pb_sync_time")
+        self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(0, 0, 361, 331))
+        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        spacerItem = QtWidgets.QSpacerItem(78, 20, QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem)
+        spacerItem1 = QtWidgets.QSpacerItem(17, 328, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
+        self.horizontalLayout.addItem(spacerItem1)
+        spacerItem2 = QtWidgets.QSpacerItem(78, 20, QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem2)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 363, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.retranslateUi(MainWindow)
+        self.pb_go_back.clicked.connect(self.go_back)
+        self.pb_sync_time.clicked.connect(self.sync_time)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def go_back(self):
+        print('pb go back clicked')
+        pb_go_back_status = 1
+        print('pb go back status = 1')
+        self.PT = ProcessThread(self)
+        self.PT.start()
+        print('start threath')
+    def sync_time(self):
+        pb_sync_time_status = 1
+        self.PT = ProcessThread(self)
+        self.PT.start()
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pb_go_back.setToolTip(_translate("MainWindow", "<div>\n"
+"    <style scoped>\n"
+"\n"
+"        .button-success,\n"
+" {\n"
+"            color: white;\n"
+"            border-radius: 4px;\n"
+"            text-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);\n"
+"        }\n"
+"\n"
+"        .button-success {\n"
+"            background: rgb(28, 184, 65); /* this is a green */\n"
+"        }\n"
+"\n"
+"\n"
+"    </style>\n"
+"\n"
+"    <button class=\"button-success pure-button\">Success Button</button>\n"
+"</div>"))
+        self.pb_go_back.setText(_translate("MainWindow", "Go back to 2005"))
+        self.pb_sync_time.setText(_translate("MainWindow", "Sync Time"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
